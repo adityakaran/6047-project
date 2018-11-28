821c961608ea02b0e416805ebd566c8158c98063
Yang Dai <ydai144@gmail.com>
2018-11-28 14:41:35 -0500

    added helper file

---
+++ source/helpers.py
@@ -0,0 +1,50 @@
+import sys
+
+
+# returns proportion of genes that are in the interactome
+def findGenes(genes, gene_dict):
+	count = 0
+	for g in genes:
+		if g in gene_dict:
+			count += 1
+	return count / float(len(genes))
+
+
+def readInteractome(interactome_file):
+	gene_dict = {}
+	file = open(interactome_file, 'r')
+	for line in file:
+		if not line.startswith('#'):
+			split_line = line.split()
+			gene1 = split_line[0]
+			gene2 = split_line[1]
+			if gene1 not in gene_dict:
+				gene_dict[gene1] = 0
+			if gene2 not in gene_dict:
+				gene_dict[gene2] = 0
+			gene_dict[gene1] += 1
+			gene_dict[gene2] += 1
+	return gene_dict
+
+def readGenes(gene_file):
+	gene_list = []
+	file = open(gene_file, 'r')
+	for line in file:
+		gene_list.append(line.strip())
+	return gene_list
+
+
+def main():
+	interactome_file = 'interactome.tsv'
+	gene_file = sys.argv[1]
+
+	gene_dict = readInteractome(interactome_file)
+	gene_list = readGenes(gene_file)
+
+	# print gene_dict
+	print len(gene_list)
+
+	print findGenes(gene_list, gene_dict)
+
+if __name__ == '__main__':
+    main()
